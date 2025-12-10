8971475d2672ac9b7a49813695003e802666cd35
Andras Pinter <andras@pinter.io>
2025-12-10 19:06:54 +0100

    f(pinter.io): switch to Svelte

---
+++ pinter.io/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import construction from '$lib/assets/construction.svg';
+
+	let title = 'Under construction';
+</script>
+
+<div class="content" role="main">
+	<img src={construction} width="50%" height="50%" alt="Under construction" />
+	<h1>
+		<span>{title}</span>
+	</h1>
+</div>
+
+<style>
+	:host {
+		font-family:
+			-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
+			'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+		font-size: 14px;
+		color: #333;
+		box-sizing: border-box;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+
+	.content {
+		display: flex;
+		margin: 82px auto 32px;
+		padding: 0 16px;
+		max-width: 960px;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
